87d2ff73ad8a01e463adcb1e4f274b0ee2ada8a2
corexalt <corexalt@gmail.com>
2024-01-31 18:21:17 +0700

    Improve event pages and sidebar

--- src/components/shared/AdminSidebar.tsx
+++ src/components/shared/AdminSidebar.tsx
@@ -5,43 +5,45 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/
 
 const AdminSidebar = () => {
   return (
-    <nav className="relative min-w-[80px] bg-blue-950 border-r px-3 pb-10 pt-8">
-        <div className="flex flex-col items-center">
-          <img
-            alt="Logo"
-            className="mb-8 w-12 h-12"
-            height="50"
-            src="/logo_white.svg"
-            style={{
-              aspectRatio: "50/50",
-              objectFit: "cover",
-            }}
-            width="50"
-          />
-          <div className="flex flex-col gap-6">
+    <nav className="relative min-h-screen bg-blue-950 border-r md:w-72 px-4 pb-10">
+        <div className="flex flex-col items-start">
+          <div className="p-2 rounded-2xl">
+            <img
+              alt="Logo"
+              className="w-36 h-36"
+              height="50"
+              src="/corexalt_typo.svg"
+              style={{
+                aspectRatio: "16/9",
+                objectFit: "contain",
+              }}
+              width="50"
+            />
+          </div>
+          <div className="flex flex-col gap-8 w-full">
             <Link
-              className="flex items-center text-white gap-2 text-sm font-medium md:text-sm hover:bg-gray-300 dark:hover:bg-gray-700 rounded-lg p-2"
+              className="flex items-center text-white focus:text-[#F2FF5F] gap-4 text-sm font-medium md:text-sm hover:bg-[#1e316b] focus:bg-[#1e316b] rounded-lg p-2"
               href="/admin"
             >
               <HomeIcon className="w-4 h-4 dark:text-gray-400" />
               <span className="hidden lg:block">Home</span>
             </Link>
             <Link
-              className="flex items-center text-white gap-2 text-sm font-medium md:text-sm hover:bg-gray-300 dark:hover:bg-gray-700 rounded-lg p-2"
+              className="flex items-center text-white focus:text-[#F2FF5F] gap-4 text-sm font-medium md:text-sm hover:bg-[#1e316b] focus:bg-[#1e316b] rounded-lg p-2"
               href="/admin/attendees"
             >
               <UsersIcon className="w-4 h-4 dark:text-gray-400" />
               <span className="hidden lg:block">Attendees</span>
             </Link>
             <Link
-              className="flex items-center text-white gap-2 text-sm font-medium md:text-sm hover:bg-gray-300 dark:hover:bg-gray-700 rounded-lg p-2"
+              className="flex items-center text-white focus:text-[#F2FF5F] gap-4 text-sm font-medium md:text-sm hover:bg-[#1e316b] focus:bg-[#1e316b] rounded-lg p-2"
               href="/admin/events"
             >
               <CalendarIcon className="w-4 h-4 dark:text-gray-400" />
               <span className="hidden lg:block">Events</span>
             </Link>
             <Link
-              className="flex items-center text-white gap-2 text-sm font-medium md:text-sm hover:bg-gray-300 dark:hover:bg-gray-700 rounded-lg p-2"
+              className="flex items-center text-white focus:text-[#F2FF5F] gap-4 text-sm font-medium md:text-sm hover:bg-[#1e316b] focus:bg-[#1e316b] rounded-lg p-2"
               href="/admin/mentors"
             >
               <GroupIcon className="w-4 h-4 dark:text-gray-400" />
