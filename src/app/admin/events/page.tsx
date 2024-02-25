053d47e6026683e3871386875e7d04ab51d51e9b
durianbeetroot <durianbeetroot@gmail.com>
2024-02-25 11:25:58 +0700

    display events

--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -1,20 +1,50 @@
+"use client"
+
 import { DataTable } from "@/components/shared/DataTable"
 import { EventsColumns } from "./columns"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { useEffect, useState } from "react"
+import { supabase } from "@/lib/initSupabase"
+import { table } from "console"
 
-const data = [
-  {
-    name: "something",
-    host: "Bobby and Ravi",
-    description: "Cool event",
-    imageUrl: "google.com",
-    date: "10 Dec 2024"
-  }
-]
+// const data = [
+//   {
+//     name: "something",
+//     host: "Bobby and Ravi",
+//     description: "Cool event",
+//     imageUrl: "google.com",
+//     date: "10 Dec 2024"
+//   },
+// ]
 
 const page = () => {
+
+  const [events, setEvents] = useState<any>([]);
+
+  useEffect(() => {
+    // Function to fetch data from Supabase
+    const fetchEvents = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('event')
+          .select('*');
+
+        if (error) {
+          throw error;
+        }
+
+        setEvents(data);
+      } catch (error:any) {
+        console.error('Error fetching events:', error.message);
+      }
+    };
+
+    // Call the fetchEvents function
+    fetchEvents();
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
     return (
       <section className="w-full">
         <div className="flex items-center py-4">
@@ -28,9 +58,9 @@ const page = () => {
             </Link>
           </Button>
         </div>
-        {/* <DataTable columns={ EventsColumns } data={ data } /> */}
+        <DataTable columns={ EventsColumns } data={ events } />
       </section>
     )
   }
   
-  export default page
+  export default page;
